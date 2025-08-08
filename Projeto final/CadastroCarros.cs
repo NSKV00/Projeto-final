db9e1330eedc13ccc3bb418b1c73ef28eabc8540
starshpping333 <starshopping3333@gmail.com>
2025-08-07 22:02:07 -0300

    cadastro de veiculos

---
+++ Projeto final/CadastroCarros.cs
@@ -0,0 +1,91 @@
+using MySqlConnector;
+using System.Text.RegularExpressions;
+
+namespace Projeto_final
+{
+    public partial class CadastroCarros : Form
+    {
+        public CadastroCarros()
+        {
+            InitializeComponent();
+
+
+        }
+
+        private bool ValidarPlaca(string placa)
+        {
+            placa = placa.Trim().ToUpper();
+
+            Regex regexMercosul = new Regex(@"^[A-Z]{3}[0-9][A-Z][0-9]{2}$");
+
+            return regexMercosul.IsMatch(placa);
+        }
+
+        private void Textplaca_Leave(object sender, EventArgs e)
+        {
+            if (!ValidarPlaca(Textplaca.Text))
+            {
+                MessageBox.Show("Placa inválida. O formato correto é ABC1D23 (3 letras, 1 número, 1 letra, 2 números).");
+                Textplaca.Focus();
+            }
+        }
+
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+            if(string.IsNullOrWhiteSpace(Textnamecliente.Text) ||
+               string.IsNullOrWhiteSpace(Textmarca.Text) ||
+               string.IsNullOrWhiteSpace(Textmodelo.Text) ||
+               string.IsNullOrWhiteSpace(Textplaca.Text) ||
+               string.IsNullOrWhiteSpace(Textanodefab.Text))
+            {
+                MessageBox.Show("Por favor, preencha todos os campos.");
+                return;
+            }
+
+            if(!ValidarPlaca(Textplaca.Text))
+            {
+                MessageBox.Show("Placa inválida. O formato correto é ABC1D23 (3 letras, 1 número, 1 letra, 2 números).");
+                return;
+            }
+
+            try
+            {
+                 using (var conexao = Conexao.Conexao.ObterConexao())
+                {
+                    string sql = @"INSERT INTO veiculos (nome_cliente, marca, modelo, placa, ano_fabricacao) 
+                        VALUES (@nome_cliente, @marca, @modelo, @placa, @ano_fabricacao)";                                    
+                         
+
+                   MySqlCommand cmd = new MySqlCommand(sql, conexao);
+                    {
+                        cmd.Parameters.AddWithValue("@nome_cliente", Textnamecliente.Text);
+                        cmd.Parameters.AddWithValue("@marca", Textmarca.Text);
+                        cmd.Parameters.AddWithValue("@modelo", Textmodelo.Text);
+                        cmd.Parameters.AddWithValue("@placa", Textplaca.Text);
+                        cmd.Parameters.AddWithValue("@ano_fabricacao", int.Parse(Textanodefab.Text));
+                        
+                        
+                        cmd.ExecuteNonQuery();
+
+                        MessageBox.Show("Dados salvos com sucesso!");
+
+                        Textnamecliente.Clear();
+                        Textmarca.Clear();
+                        Textmodelo.Clear();
+                        Textplaca.Clear();
+                        Textanodefab.Clear();
+                        
+                    }
+                }
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Erro ao salvar os dados: {ex.Message}");
+                return;
+            }
+
+
+        }
+    }
+}
---
+++ Projeto final/Models/CadastroCarros.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_final.Models
+{
+    internal class CadastroCarros
+    {
+
+        public string NomeCliente { get; set; }
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public string Placa { get; set; }
+        public int AnoDeFabricacao { get; set; }
+
+    }
+}
