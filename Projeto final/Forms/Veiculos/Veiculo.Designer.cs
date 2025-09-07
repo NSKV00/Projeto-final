5072f36fe0862c89b2cb32b05fba37003409a64a
NSKV00 <nicolas.g.g.dsilva@gmail.com>
2025-09-07 18:06:02 -0300

    ajuste veiculos

--- Projeto final/Forms/Veiculos/Veiculo.Designer.cs
+++ Projeto final/Forms/Veiculos/Veiculo.Designer.cs
@@ -1,73 +1,21 @@
-﻿//namespace Projeto_final.Forms.Veiculos
-//{
-//    partial class Veiculo
-//    {
-//        /// <summary>
-//        /// Required designer variable.
-//        /// </summary>
-//        private System.ComponentModel.IContainer components = null;
-
-//        /// <summary>
-//        /// Clean up any resources being used.
-//        /// </summary>
-//        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-//        protected override void Dispose(bool disposing)
-//        {
-//            if (disposing && (components != null))
-//            {
-//                components.Dispose();
-//            }
-//            base.Dispose(disposing);
-//        }
-
-//        #region Windows Form Designer generated code
-
-//        /// <summary>
-//        /// Required method for Designer support - do not modify
-//        /// the contents of this method with the code editor.
-//        /// </summary>
-//        private void InitializeComponent()
-//        {
-//            this.components = new System.ComponentModel.Container();
-//            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-//            this.ClientSize = new System.Drawing.Size(800, 450);
-//            this.Text = "Veiculo";
-//        }
-
-//        #endregion
-//    }
-//}
-using static System.Net.Mime.MediaTypeNames;
+﻿using static System.Net.Mime.MediaTypeNames;
 using System.Xml.Linq;
 
 namespace Projeto_final.Forms.Veiculos
 {
     partial class Veiculo
     {
-        /// <summary>
-        ///  Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        ///  Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
-            {
                 components.Dispose();
-            }
             base.Dispose(disposing);
         }
 
         #region Windows Form Designer generated code
 
-        /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
             Textnamecliente = new TextBox();
@@ -75,133 +23,134 @@ namespace Projeto_final.Forms.Veiculos
             label2 = new Label();
             Textmarca = new TextBox();
             label3 = new Label();
-            label4 = new Label();
             Textmodelo = new TextBox();
+            label4 = new Label();
             Textplaca = new TextBox();
             label5 = new Label();
             Textanodefab = new TextBox();
             btnSalvar = new Button();
-            button1 = new Button();
+
             SuspendLayout();
+
+            // 
+            // label1 - CPF do Cliente
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(40, 40);
+            label1.Name = "label1";
+            label1.Size = new Size(120, 20);
+            label1.Font = new System.Drawing.Font("Segoe UI", 10, FontStyle.Regular);
+            label1.Text = "CPF do Proprietário";
+
             // 
             // Textnamecliente
             // 
-            Textnamecliente.Location = new Point(181, 93);
-            Textnamecliente.Name = "Textnamecliente";
-            Textnamecliente.Size = new Size(156, 27);
-            Textnamecliente.TabIndex = 0;
+            Textnamecliente.Location = new Point(40, 65);
+            Textnamecliente.Size = new Size(200, 27);
+            Textnamecliente.Font = new System.Drawing.Font("Segoe UI", 10);
+
             // 
-            // label1
-            // 
-            label1.AutoSize = true;
-            label1.Location = new Point(181, 58);
-            label1.Name = "label1";
-            label1.Size = new Size(138, 20);
-            label1.TabIndex = 1;
-            label1.Text = "Cpf do proprietario";
-            label1.Click += label1_Click;
-            // 
-            // label2
+            // label2 - Marca
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(181, 148);
+            label2.Location = new Point(40, 105);
             label2.Name = "label2";
-            label2.Size = new Size(110, 20);
-            label2.TabIndex = 2;
-            label2.Text = "Marca do carro";
+            label2.Size = new Size(95, 20);
+            label2.Font = new System.Drawing.Font("Segoe UI", 10);
+            label2.Text = "Marca do Carro";
+
             // 
             // Textmarca
             // 
-            Textmarca.Location = new Point(181, 171);
-            Textmarca.Name = "Textmarca";
-            Textmarca.Size = new Size(156, 27);
-            Textmarca.TabIndex = 3;
+            Textmarca.Location = new Point(40, 130);
+            Textmarca.Size = new Size(200, 27);
+            Textmarca.Font = new System.Drawing.Font("Segoe UI", 10);
+
             // 
-            // label3
+            // label3 - Modelo
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(181, 232);
+            label3.Location = new Point(40, 170);
             label3.Name = "label3";
-            label3.Size = new Size(121, 20);
-            label3.TabIndex = 4;
-            label3.Text = "Modelo do carro";
-            // 
-            // label4
-            // 
-            label4.AutoSize = true;
-            label4.Location = new Point(521, 148);
-            label4.Name = "label4";
-            label4.Size = new Size(117, 20);
-            label4.TabIndex = 5;
-            label4.Text = "Placa do veículo";
+            label3.Size = new Size(95, 20);
+            label3.Font = new System.Drawing.Font("Segoe UI", 10);
+            label3.Text = "Modelo do Carro";
+
             // 
             // Textmodelo
             // 
-            Textmodelo.Location = new Point(181, 255);
-            Textmodelo.Name = "Textmodelo";
-            Textmodelo.Size = new Size(156, 27);
-            Textmodelo.TabIndex = 6;
+            Textmodelo.Location = new Point(40, 195);
+            Textmodelo.Size = new Size(200, 27);
+            Textmodelo.Font = new System.Drawing.Font("Segoe UI", 10);
+
+            // 
+            // label4 - Placa
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(280, 40);
+            label4.Name = "label4";
+            label4.Size = new Size(90, 20);
+            label4.Font = new System.Drawing.Font("Segoe UI", 10);
+            label4.Text = "Placa Veículo";
+
             // 
             // Textplaca
             // 
-            Textplaca.Location = new Point(521, 171);
-            Textplaca.Name = "Textplaca";
-            Textplaca.Size = new Size(156, 27);
-            Textplaca.TabIndex = 7;
+            Textplaca.Location = new Point(280, 65);
+            Textplaca.Size = new Size(150, 27);
+            Textplaca.Font = new System.Drawing.Font("Segoe UI", 10);
+
             // 
-            // label5
+            // label5 - Ano de Fabricação
             // 
             label5.AutoSize = true;
-            label5.Location = new Point(521, 58);
+            label5.Location = new Point(280, 105);
             label5.Name = "label5";
-            label5.Size = new Size(131, 20);
-            label5.TabIndex = 8;
-            label5.Text = "Ano de fabricação";
+            label5.Size = new Size(120, 20);
+            label5.Font = new System.Drawing.Font("Segoe UI", 10);
+            label5.Text = "Ano de Fabricação";
+
             // 
             // Textanodefab
             // 
-            Textanodefab.Location = new Point(521, 93);
-            Textanodefab.Name = "Textanodefab";
-            Textanodefab.Size = new Size(156, 27);
-            Textanodefab.TabIndex = 9;
+            Textanodefab.Location = new Point(280, 130);
+            Textanodefab.Size = new Size(150, 27);
+            Textanodefab.Font = new System.Drawing.Font("Segoe UI", 10);
+
             // 
             // btnSalvar
             // 
-            btnSalvar.BackColor = Color.DarkGray;
-            btnSalvar.Location = new Point(565, 255);
-            btnSalvar.Name = "btnSalvar";
-            btnSalvar.Size = new Size(112, 39);
-            btnSalvar.TabIndex = 10;
-            btnSalvar.Text = "Salvar";
+            btnSalvar.Location = new Point(280, 180);
+            btnSalvar.Size = new Size(150, 35);
+            btnSalvar.BackColor = Color.LightSteelBlue;
+            btnSalvar.ForeColor = Color.Black;
+            btnSalvar.Font = new System.Drawing.Font("Segoe UI", 10, FontStyle.Bold);
+            btnSalvar.Text = "Salvar Veículo";
             btnSalvar.UseVisualStyleBackColor = false;
             btnSalvar.Click += btnSalvar_Click;
+
             // 
-            // button1
-            // 
-            button1.Location = new Point(0, 0);
-            button1.Name = "button1";
-            button1.Size = new Size(75, 23);
-            button1.TabIndex = 0;
-            // 
-            // CadastroCarros
+            // Veiculo Form
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1367, 499);
-            Controls.Add(button1);
-            Controls.Add(btnSalvar);
-            Controls.Add(Textanodefab);
-            Controls.Add(label5);
-            Controls.Add(Textplaca);
-            Controls.Add(Textmodelo);
-            Controls.Add(label4);
-            Controls.Add(label3);
-            Controls.Add(Textmarca);
-            Controls.Add(label2);
+            ClientSize = new Size(500, 280);
             Controls.Add(label1);
             Controls.Add(Textnamecliente);
-            Name = "CadastroCarros";
-            Text = "Form1";
+            Controls.Add(label2);
+            Controls.Add(Textmarca);
+            Controls.Add(label3);
+            Controls.Add(Textmodelo);
+            Controls.Add(label4);
+            Controls.Add(Textplaca);
+            Controls.Add(label5);
+            Controls.Add(Textanodefab);
+            Controls.Add(btnSalvar);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MaximizeBox = false;
+            Name = "Veiculo";
+            Text = "Cadastro de Veículos";
+            Load += Veiculo_Load;
             ResumeLayout(false);
             PerformLayout();
         }
@@ -213,12 +162,11 @@ namespace Projeto_final.Forms.Veiculos
         private Label label2;
         private TextBox Textmarca;
         private Label label3;
-        private Label label4;
         private TextBox Textmodelo;
+        private Label label4;
         private TextBox Textplaca;
         private Label label5;
         private TextBox Textanodefab;
         private Button btnSalvar;
-        private Button button1;
     }
 }
--- Projeto final/Forms/Veiculos/Veiculo.cs
+++ Projeto final/Forms/Veiculos/Veiculo.cs
@@ -1,27 +1,7 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.ComponentModel;
-//using System.Data;
-//using System.Drawing;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
-//using System.Windows.Forms;
-
-//namespace Projeto_final.Forms.Veiculos
-//{
-//    public partial class Veiculo : Form
-//    {
-//        public Veiculo()
-//        {
-//            InitializeComponent();
-//        }
-//    }
-//}
-
-using MySql.Data.MySqlClient;
+﻿using MySql.Data.MySqlClient;
 using SistemaDatabase.Conexoes;
 using System.Text.RegularExpressions;
+using System.Windows.Forms;
 
 namespace Projeto_final.Forms.Veiculos
 {
@@ -30,16 +10,12 @@ namespace Projeto_final.Forms.Veiculos
         public Veiculo()
         {
             InitializeComponent();
-
-
         }
 
         private bool ValidarPlaca(string placa)
         {
             placa = placa.Trim().ToUpper();
-
             Regex regexMercosul = new Regex(@"^[A-Z]{3}[0-9][A-Z][0-9]{2}$");
-
             return regexMercosul.IsMatch(placa);
         }
 
@@ -48,17 +24,16 @@ namespace Projeto_final.Forms.Veiculos
             if (!ValidarPlaca(Textplaca.Text))
             {
                 MessageBox.Show("Placa inválida. O formato correto é ABC1D23 (3 letras, 1 número, 1 letra, 2 números).");
-                Textplaca.Focus();
             }
         }
 
         private void btnSalvar_Click(object sender, EventArgs e)
         {
             if (string.IsNullOrWhiteSpace(Textnamecliente.Text) ||
-               string.IsNullOrWhiteSpace(Textmarca.Text) ||
-               string.IsNullOrWhiteSpace(Textmodelo.Text) ||
-               string.IsNullOrWhiteSpace(Textplaca.Text) ||
-               string.IsNullOrWhiteSpace(Textanodefab.Text))
+                string.IsNullOrWhiteSpace(Textmarca.Text) ||
+                string.IsNullOrWhiteSpace(Textmodelo.Text) ||
+                string.IsNullOrWhiteSpace(Textplaca.Text) ||
+                string.IsNullOrWhiteSpace(Textanodefab.Text))
             {
                 MessageBox.Show("Por favor, preencha todos os campos.");
                 return;
@@ -70,20 +45,20 @@ namespace Projeto_final.Forms.Veiculos
                 return;
             }
 
+            if (!int.TryParse(Textanodefab.Text, out int anoFab))
+            {
+                MessageBox.Show("Ano de fabricação inválido. Informe um número inteiro.");
+                return;
+            }
 
             try
             {
                 using (var conexao = Conexao.ObterConexao())
                 {
-                    string sql = @"INSERT INTO veiculos (cpf_cliente, marca, modelo, placa, ano_fabricacao) 
-                        VALUES (@cpf_cliente, @marca, @modelo, @placa, @ano_fabricacao)";
-
                     string queryCliente = "SELECT id, nome FROM cliente WHERE cpf = @cpf_cliente";
-
                     using (var checkCmd = new MySqlCommand(queryCliente, conexao))
                     {
                         checkCmd.Parameters.AddWithValue("@cpf_cliente", Textnamecliente.Text);
-
                         using (var reader = checkCmd.ExecuteReader())
                         {
                             if (!reader.HasRows)
@@ -94,42 +69,37 @@ namespace Projeto_final.Forms.Veiculos
                         }
                     }
 
-                    MySqlCommand cmd = new MySqlCommand(sql, conexao);
+                    string sql = @"INSERT INTO veiculos (cpf_cliente, marca, modelo, placa, ano_fabricacao) 
+                                   VALUES (@cpf_cliente, @marca, @modelo, @placa, @ano_fabricacao)";
+                    using (var cmd = new MySqlCommand(sql, conexao))
                     {
-
                         cmd.Parameters.AddWithValue("@cpf_cliente", Textnamecliente.Text);
                         cmd.Parameters.AddWithValue("@marca", Textmarca.Text);
                         cmd.Parameters.AddWithValue("@modelo", Textmodelo.Text);
-                        cmd.Parameters.AddWithValue("@placa", Textplaca.Text);
-                        cmd.Parameters.AddWithValue("@ano_fabricacao", int.Parse(Textanodefab.Text));
-
+                        cmd.Parameters.AddWithValue("@placa", Textplaca.Text.Trim().ToUpper());
+                        cmd.Parameters.AddWithValue("@ano_fabricacao", anoFab);
 
                         cmd.ExecuteNonQuery();
-
-                        MessageBox.Show("Dados salvos com sucesso!");
-
-                        Textnamecliente.Clear();
-                        Textmarca.Clear();
-                        Textmodelo.Clear();
-                        Textplaca.Clear();
-                        Textanodefab.Clear();
-
                     }
+
+                    MessageBox.Show("Dados salvos com sucesso!");
+
+                    Textnamecliente.Clear();
+                    Textmarca.Clear();
+                    Textmodelo.Clear();
+                    Textplaca.Clear();
+                    Textanodefab.Clear();
                 }
             }
-
             catch (Exception ex)
             {
                 MessageBox.Show($"Erro ao salvar os dados: {ex.Message}");
-                return;
             }
-
-
         }
 
-        private void label1_Click(object sender, EventArgs e)
+        private void Veiculo_Load(object sender, EventArgs e)
         {
-
+     
         }
     }
 }
